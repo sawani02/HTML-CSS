4cbcb17e8a702a02dcee4504eb79b9f10847a76e
Sawani Soman <sawani.soman92@gmail.com>
2017-09-04 23:21:31 -0700

    Add files via upload

---
+++ name_map.js
@@ -0,0 +1,157 @@
+var name_map = {
+        "US-VA": {
+            "name": "Virginia"
+        },
+        "US-PA": {
+            "name": "Pennsylvania"
+        },
+        "US-TN": {
+            "name": "Tennessee"
+        },
+        "US-WV": {
+            "name": "West Virginia"
+        },
+        "US-NV": {
+            "name": "Nevada"
+        },
+        "US-TX": {
+            "name": "Texas"
+        },
+        "US-NH": {
+            "name": "New Hampshire"
+        },
+        "US-NY": {
+            "name": "New York"
+        },
+        "US-HI": {
+            "name": "Hawaii"
+        },
+        "US-VT": {
+            "name": "Vermont"
+        },
+        "US-NM": {
+            "name": "New Mexico"
+        },
+        "US-NC": {
+            "name": "North Carolina"
+        },
+        "US-ND": {
+            "name": "North Dakota"
+        },
+        "US-NE": {
+            "name": "Nebraska"
+        },
+        "US-LA": {
+            "name": "Louisiana"
+        },
+        "US-SD": {
+            "name": "South Dakota"
+        },
+        "US-DC": {
+            "name": "District of Columbia"
+        },
+        "US-DE": {
+            "name": "Delaware"
+        },
+        "US-FL": {
+            "name": "Florida"
+        },
+        "US-CT": {
+            "name": "Connecticut"
+        },
+        "US-WA": {
+            "name": "Washington"
+        },
+        "US-KS": {
+            "name": "Kansas"
+        },
+        "US-WI": {
+            "name": "Wisconsin"
+        },
+        "US-OR": {
+            "name": "Oregon"
+        },
+        "US-KY": {
+            "name": "Kentucky"
+        },
+        "US-ME": {
+            "name": "Maine"
+        },
+        "US-OH": {
+            "name": "Ohio"
+        },
+        "US-OK": {
+            "name": "Oklahoma"
+        },
+        "US-ID": {
+            "name": "Idaho"
+        },
+        "US-WY": {
+            "name": "Wyoming"
+        },
+        "US-UT": {
+            "name": "Utah"
+        },
+        "US-IN": {
+            "name": "Indiana"
+        },
+        "US-IL": {
+            "name": "Illinois"
+        },
+        "US-AK": {
+            "name": "Alaska"
+        },
+        "US-NJ": {
+            "name": "New Jersey"
+        },
+        "US-CO": {
+            "name": "Colorado"
+        },
+        "US-MD": {
+            "name": "Maryland"
+        },
+        "US-MA": {
+            "name": "Massachusetts"
+        },
+        "US-AL": {
+            "name": "Alabama"
+        },
+        "US-MO": {
+            "name": "Missouri"
+        },
+        "US-MN": {
+            "name": "Minnesota"
+        },
+        "US-CA": {
+            "name": "California"
+        },
+        "US-IA": {
+            "name": "Iowa"
+        },
+        "US-MI": {
+            "name": "Michigan"
+        },
+        "US-GA": {
+            "name": "Georgia"
+        },
+        "US-AZ": {
+            "name": "Arizona"
+        },
+        "US-MT": {
+            "name": "Montana"
+        },
+        "US-MS": {
+            "name": "Mississippi"
+        },
+        "US-SC": {
+            "name": "South Carolina"
+        },
+        "US-RI": {
+            "name": "Rhode Island"
+        },
+        "US-AR": {
+            "name": "Arkansas"
+        }
+}
+
+
